b299b1b997a4eb831683653e771a4ce72bf0ec14
RichardLourie <32876317+RichardLourie@users.noreply.github.com>
2023-10-18 23:17:20 -0400

    Update add_song_page.php

--- add_song_page.php
+++ add_song_page.php
@@ -12,17 +12,17 @@
 
   <p>
     <label> Artist: </label>
-    <input type="text" id="artist" name="artist" />
+    <input type="text" id="artist" name="artist" required/>
   </p>
 
   <p>
     <label> Song: </label>
-    <input type="text" id="song" name="song" />
+    <input type="text" id="song" name="song" required/>
   </p>
 
   <p>
     <label> Rating (1-5): </label>
-    <input type="number" id="rating" name="rating" min="1" max="5"/>
+    <input type="number" id="rating" name="rating" min="1" max="5" required/>
   </p>
 
   <p>
